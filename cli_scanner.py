e32d88e4b5a52b28d5215ea7139f6b2f147478f8
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-02-07 22:24:00 +0500

    cli_scanner updated to scan ports rather than dummy scan testing

--- cli_scanner.py
+++ cli_scanner.py
@@ -1,13 +1,13 @@
 import argparse
 from cli_scanner.core.runner import ScanRunner
-from cli_scanner.scanners.dummy_scanner import DummyScanner
+from cli_scanner.scanners.port_scanner import PortScanner
 
 def main():
     parser = argparse.ArgumentParser(description = "CLI Linux Security Scanner")
     parser.add_argument("--verbose", action = "store_true")
     args = parser.parse_args()
 
-    scanners = [DummyScanner(verbose = args.verbose)]
+    scanners = [PortScanner(verbose = args.verbose)]
     runner = ScanRunner(scanners, verbose = args.verbose)
 
     results = runner.run()
